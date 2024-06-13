3f5b2225a7b0fc9e34c7dcec9b4094585fd8f404
Lukas Havemann <havemann.lukas@gmail.com>
2024-06-13 16:12:48 +0200

    update

--- keyboards/keychron/k9_pro/ansi/white/keymaps/lhavemann/keymap.c
+++ keyboards/keychron/k9_pro/ansi/white/keymaps/lhavemann/keymap.c
@@ -24,13 +24,21 @@ enum layers{
     L_FN1,
 };
 
+
+// Keycodes for umlauts and eszett
+#define UML_AE RALT(KC_Q)
+#define UML_OE RALT(KC_P)
+#define UML_UE RALT(KC_Y)
+#define GER_SZ RALT(KC_S)
+#define EU_EUR RALT(KC_5)
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_61_ansi(
         KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,  KC_BSPC,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC, KC_BSLS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Z,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC, KC_BSLS,
         KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
+        KC_LSFT,            KC_Y,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                     LT(L_FN1, KC_SPC),                                 KC_RCMMD,MO(MAC_FN),MO(L_FN1),KC_RCTL),
 
     [WIN_BASE] = LAYOUT_61_ansi(
@@ -55,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
     [L_FN1] = LAYOUT_61_ansi(
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        KC_CAPS,  _______,  KC_LBRC, RSFT(KC_LBRC),  RSFT(KC_9),  _______,  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RIGHT,  _______,  _______,            _______,
+        KC_GRV ,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,
+        _______,  _______,  EU_EUR,  _______,  _______,  _______,  _______,  LOPT(KC_U),  _______,  UML_OE,  _______,  _______,  _______,  _______,
+        KC_CAPS,  _______, RALT(KC_S), KC_LBRC, RSFT(KC_LBRC),  RSFT(KC_9),  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RIGHT,  _______, _______, _______,
         _______,            _______,  _______,  _______,  _______,  BAT_LVL,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 };
